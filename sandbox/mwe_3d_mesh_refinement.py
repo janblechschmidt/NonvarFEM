ab551943fc21815a97e8f1b727fc752e641b64c6
Max Winkler <max.winkler@mathematik.tu-chemnitz.de>
2020-07-15 14:31:07 +0200

    Extended test

--- sandbox/mwe_3d_mesh_refinement.py
+++ sandbox/mwe_3d_mesh_refinement.py
@@ -1,25 +1,22 @@
 from dolfin import *
 import mshr
-n = 2
-p = 3
+
 dom = mshr.Box(Point(0., 0., 0.), Point(1., 1., 1.))
-m = mshr.generate_mesh(dom, n)
-# , "cgal")
+m_orig = mshr.generate_mesh(dom,0.01)
 
-# m = BoxMesh(Point(0., 0., 0.), Point(1., 1., 1.), n, n, n)
+xf = File('mesh_orig.pvd')
+xf << m_orig
 
-V = FunctionSpace(m, 'Lagrange', p)
+m = refine(m_orig)
+
+V = FunctionSpace(m, 'Lagrange', 2)
 u = TrialFunction(V)
 v = TestFunction(V)
 
 M = assemble(u * v * dx)
+
 M_diag = as_backend_type(M).mat().getDiagonal().array
 print('Min of diag of M: {}'.format(M_diag.min()))
 
-m = refine(m)
-V = FunctionSpace(m, 'Lagrange', p)
-u = TrialFunction(V)
-v = TestFunction(V)
-M = assemble(u * v * dx)
-M_diag = as_backend_type(M).mat().getDiagonal().array
-print('Min of diag of M: {}'.format(M_diag.min()))
+xf = File('mesh.pvd')
+xf << m
